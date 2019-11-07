8892132dc381d71a9a652da601c8f81c80e2bf5b
bRozenfeld <48674552+bRozenfeld@users.noreply.github.com>
2019-11-07 17:25:05 +0100

    convert the data send to json

--- gui/app/app.js
+++ gui/app/app.js
@@ -1,4 +1,6 @@
 window.addEventListener("unload", function() {
   var id = document.getElementById("id");
-  navigator.sendBeacon("http://localhost/FalconNews/api/news/update_display.php");
+  navigator.sendBeacon("http://localhost/FalconNews/api/news/update_display.php", JSON.stringify({
+    id : id.innerHTML
+  });
 });

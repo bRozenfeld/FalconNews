ed1ec9a9096955d2b4560b20c3c7c0d4d0d9fd61
bRozenfeld <48674552+bRozenfeld@users.noreply.github.com>
2019-11-07 16:07:43 +0100

    add core.php

---
+++ api/config/core.php
@@ -0,0 +1,11 @@
+<?php
+// set default time zone
+date_default_timezone_set('Europe/Paris');
+
+// variables used for jwt;
+$key = "private key RSA";
+$iss = "http://localhost/FalconNews";
+$aud = "http://localhost/FalconNews/";
+$iat = time();
+$nbf = $iat + 2;
+?>
